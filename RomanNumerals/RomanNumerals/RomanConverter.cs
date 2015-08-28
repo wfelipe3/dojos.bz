58cbfe4393de7aec00c76c73d6e11a3db93e102a
williame <william.rojas@bizagi.com>
2015-08-28 09:43:20 -0500

    DOJOS.BZ #006 RomanNumerals

---
+++ RomanNumerals/RomanNumerals/RomanConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanConverter
+    {
+        private Dictionary<int, string> _romanConstants;
+
+        static void Main(string[] args)
+        {
+        }
+
+        public RomanConverter()
+        {
+            _romanConstants = new Dictionary<int, string>();
+            _romanConstants.Add(1, "I");
+            _romanConstants.Add(5, "V");
+            _romanConstants.Add(10, "X");
+            _romanConstants.Add(50, "L");
+            _romanConstants.Add(100, "C");
+            _romanConstants.Add(500, "D");
+            _romanConstants.Add(1000, "M");
+        }
+
+        public string ConvertToRoman(int inputNumber)
+        {
+            
+            if (_romanConstants.ContainsKey(inputNumber))
+                return _romanConstants[inputNumber];
+            else
+            {
+                if (inputNumber>89)
+                {
+                    return "XC";
+                }
+
+                if (inputNumber > 10)
+                {
+                    return "X" + ConvertToRoman(inputNumber - 10);
+                }
+                if (inputNumber > 8)
+                    return "IX";
+                if (inputNumber > 5)
+                {
+                    return "V" + ConvertToRoman(inputNumber % 5);
+                }
+
+
+                if (inputNumber > 3)
+                    return "IV";
+                return FromItoIII(inputNumber);
+            }
+        }
+
+        private static string FromItoIII(int inputNumber)
+        {
+            string result = "";
+            for (int i = 0; i < inputNumber; i++)
+            {
+                result += "I";
+            }
+            return result;
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumeralsTest/RomanConverterTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals;
+
+namespace RomanNumeralsTest
+{
+    [TestClass]
+    public class RomanConverterTest
+    {
+        
+        [TestMethod]
+        public void Given1To3ThenReturnI()
+        {
+            AssertRomanNumeral("I", 1);
+            AssertRomanNumeral("II", 2);
+            AssertRomanNumeral("III", 3);
+
+        }
+
+        [TestMethod]
+        public void Given4ThenReturnIV()
+        {
+            AssertRomanNumeral("IV",4);
+        }
+
+        [TestMethod]
+        public void Given5ThenReturnV()
+        {
+            AssertRomanNumeral("V",5);
+        }
+
+        [TestMethod]
+        public void Given10ThenReturnX()
+        {
+            AssertRomanNumeral("X",10);
+        }
+
+        [TestMethod]
+        public void Given50ThenReturnL()
+        {
+            AssertRomanNumeral("L",50);
+        }
+
+        [TestMethod]
+        public void Given100ThenReturnC()
+        {
+            AssertRomanNumeral("C",100);
+        }
+
+        [TestMethod]
+        public void Given500ThenReturnD()
+        {
+            AssertRomanNumeral("D",500);
+        }
+
+        [TestMethod]
+        public void Given1000ThenReturnM()
+        {
+            AssertRomanNumeral("M",1000);
+        }
+
+        [TestMethod]
+        public void Given6To8ThenReturnVIToVIII()
+        {
+            AssertRomanNumeral("VI",6);
+            AssertRomanNumeral("VII",7);
+            AssertRomanNumeral("VIII",8);
+        }
+
+        [TestMethod]
+        public void Given11to13ThenReturnXItoXIII()
+        {
+            AssertRomanNumeral("XI", 11);
+            AssertRomanNumeral("XII", 12);
+            AssertRomanNumeral("XIII", 13);
+        }
+
+        [TestMethod]
+        public void Given21ThenReturnXXI()
+        {
+            AssertRomanNumeral("XXI", 21);
+        }
+
+        [TestMethod]
+        public void Given9ThenReturnIX()
+        {
+            AssertRomanNumeral("IX", 9);
+        }
+
+        [TestMethod]
+        public void Given19ThenReturnXIX()
+        {
+            AssertRomanNumeral("XIX", 19);
+        }
+
+        [TestMethod]
+        public void Given99ThenReturXCIX()
+        {
+            AssertRomanNumeral("XCIX",99);
+        }
+
+        private static void AssertRomanNumeral(string expected,int given)
+        {
+            Assert.AreEqual(expected, new RomanConverter().ConvertToRoman(given));
+        }
+    }
+}
