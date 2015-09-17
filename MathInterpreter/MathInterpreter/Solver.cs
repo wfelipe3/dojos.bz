cf0ffe36939fa87f322da0691885cc6492e02afc
williame <william.rojas@bizagi.com>
2015-09-17 13:06:41 -0500

    DOJOS.BZ #007 MathInterpreter

---
+++ MathInterpreter/MathInterpreter/Solver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace MathInterpreterTest
+{
+    public class Solver
+    {
+        public static int Resolve(string expression)
+        {
+            char[] operators = new[] {'+', '-', '*'};
+            int operatorIndex = expression.IndexOf('+');
+            operatorIndex = operatorIndex == -1 ? expression.IndexOf('-') : operatorIndex;
+            operatorIndex = operatorIndex == -1 ? expression.IndexOf('*') : operatorIndex;
+            string[] parameters = expression.Split(operators);
+            if (parameters.Length == 1)
+            {
+                return ToInt(parameters[0]);
+            }
+            ICommand command = GetCommand(expression);
+            int result = ToInt(parameters[0]);
+            string parameter = expression.Substring(0, operatorIndex);
+            if (parameters.Length>1)
+            {
+                result = command.Evaluate(ToInt(parameter), expression.Substring(operatorIndex+1));
+            }
+            
+            return result;
+        }
+
+        private static ICommand GetCommand(string expression)
+        {
+            string[] parameters;
+            if (expression.Contains("-"))
+            {
+                return new SubstractionCommand();
+            }
+            else if(expression.Contains("+"))
+            {
+                return new AddCommand();
+            }
+            else
+            {
+                return new ProductCommand();
+            }
+        }
+
+        private static int ToInt(string parameter)
+        {
+            return Int32.Parse(parameter);
+        }
+
+        private interface ICommand
+        {
+            int Evaluate(int left, string right);
+        }
+
+        private class AddCommand : ICommand
+        {
+            public int Evaluate(int left, string right)
+            {
+                return left + Solver.Resolve(right);
+            }
+        }
+
+        private class SubstractionCommand : ICommand
+        {
+            public int Evaluate(int left, string right)
+            {
+                return left - Solver.Resolve(right);
+            }
+        }
+
+        private class ProductCommand : ICommand
+        {
+            public int Evaluate(int left, string right)
+            {
+                return left*Solver.Resolve(right);
+            }
+        }
+    }
+}
---
+++ MathInterpreter/MathInterpreterTest/SolverTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathInterpreterTest
+{
+    [TestClass]
+    public class SolverTest
+    {
+        private readonly Solver _solver = new Solver();
+
+        [TestMethod]
+        public void Given1Plus1ThenReturn2()
+        {
+            AssertSolver("1+1", 2);
+        }
+        [TestMethod]
+        public void Given1Plus2ThenReturn3()
+        {
+            AssertSolver("1+2", 3);
+        }
+
+        [TestMethod]
+        public void Given1Minus1ThenReturn0()
+        {
+            AssertSolver("1-1", 0);
+        }
+
+        [TestMethod]
+        public void Given1Plus1Plus1ThenReturn3()
+        {
+            AssertSolver("1+1+1", 3);
+        }
+
+        [TestMethod]
+        public void GivenTenPlusTenThenReturn20()
+        {
+            AssertSolver("10+10",20);
+        }
+
+        [TestMethod]
+        public void GivenFourAddings()
+        {
+            AssertSolver("1+2+3+4",10);
+            AssertSolver("10+20+30+40",100);
+        }
+
+        [TestMethod]
+        public void GivenXTimesYThenReturnXtimesY()
+        {
+            AssertSolver("5*6", 30);
+            AssertSolver("10*50", 500);
+        }
+
+        private void AssertSolver(string expression, int actual)
+        {
+            Assert.AreEqual(Solver.Resolve(expression), actual);
+        }
+
+    }
+
+
+}
