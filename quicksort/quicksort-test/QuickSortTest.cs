2d6bb0accfebf71a0791837a863e3b43afe6e19c
williame <william.rojas@bizagi.com>
2015-10-16 09:11:03 -0500

    DOJOS.BZ #010 QuickSort

---
+++ quicksort/quicksort-test/QuickSortTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using quicksort;
+
+namespace quicksort_test
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void SortZeroAndOneSizeList()
+        {
+            AssertQuickSort(BuildArr(), BuildArr());
+            AssertQuickSort(BuildArr(1), BuildArr(1));
+        }
+
+        [TestMethod]
+        public void SortTwoSizeList()
+        {
+            AssertQuickSort(BuildArr( 435, 56 ), BuildArr( 56 ,435));
+            AssertQuickSort(BuildArr( 435, 506 ), BuildArr( 435,506));
+            AssertQuickSort(BuildArr( 4353, 56 ), BuildArr( 56 ,4353));
+        }
+
+        [TestMethod]
+        public void SortThreeSizeList()
+        {
+            AssertQuickSort(BuildArr(435, 56,100), BuildArr(56, 100,435));
+            AssertQuickSort(BuildArr(100,56,435), BuildArr(56, 100, 435));
+            AssertQuickSort(BuildArr(56,100,435), BuildArr(56, 100, 435));
+            AssertQuickSort(BuildArr(435,100,56), BuildArr(56, 100, 435));
+        }
+
+        [TestMethod]
+        public void SortFourSizeList()
+        {
+            AssertQuickSort(BuildArr(435, 56, 100,2), BuildArr(2,56, 100, 435));
+        }
+
+        private void AssertQuickSort(int[] actual, int[] expected)
+        {
+            CollectionAssert.AreEqual(QuickSort.Sort(actual),expected);
+        }
+
+        private int[] BuildArr(params int[] values)
+        {
+            return values;
+        }
+
+    }
+}
---
+++ quicksort/quicksort/QuickSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quicksort
+{
+    public class QuickSort
+    {
+
+        public static int[] Sort(int[] actual)
+        {
+            if (actual.Length == 2)
+            {
+                if (actual[0] > actual[1])
+                {
+                    return new int[] {actual[1], actual[0]};
+                }
+            }
+            else if(actual.Length>2)
+            {
+                List<int> values = new List<int>();
+                List<int> rigth = new List<int>();
+                List<int> left = new List<int>();
+
+                int pivot = 1;
+                for (int i=0;i<actual.Length;i++)
+                {
+                    if (i!=pivot)
+                    {
+                        if (actual[i] > actual[pivot])
+                        {
+                            rigth.Add(actual[i]);
+                        }
+                        else
+                        {
+                            left.Add(actual[i]);
+                        }
+                    }
+                }
+
+                
+                values.AddRange(Sort(left.ToArray()));
+                values.Add(actual[pivot]);
+                values.AddRange(Sort(rigth.ToArray()));
+                return values.ToArray();
+            }
+            return actual;
+        }
+
+    }
+}
