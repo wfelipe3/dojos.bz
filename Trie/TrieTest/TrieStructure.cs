711e61f4e0deacd9e6d1bfc3847db0ac4babd0b2
williame <william.rojas@bizagi.com>
2015-11-30 09:46:03 -0500

    DOJOS.BZ #013 Trie

---
+++ Trie/TrieTest/TrieStructure.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrieTest
+{
+    public class TrieStructure
+    {
+        private TrieNode node = new TrieNode();
+
+        public  bool Contains(string word)
+        {
+            return node.Contains(word);
+        }
+
+        internal void Insert(string p)
+        {
+            AddTrieNode(node, p);
+        }
+        
+        class TrieNode
+        {
+            public char ActualValue {
+                get{return actualValue;}
+                set { actualValue = value; }
+            }
+            public List<TrieNode> ListTrieNodes {
+                get { return listTrieNodes; }
+                set { listTrieNodes = value; }
+            }
+
+            public char actualValue = '\0';
+            public List<TrieNode> listTrieNodes = new List<TrieNode>();
+
+            public bool Contains(string word)
+            {
+                if (string.IsNullOrEmpty(word))
+                {
+                    return actualValue == '*';
+                }
+                else
+                {
+                    if (word.ToCharArray()[0] == actualValue)
+                    {
+                        bool contained = false;
+                        string remainder = word.Substring(1);
+                        foreach (TrieNode node in ListTrieNodes)
+                        {
+                            contained = node.Contains(remainder);
+                            if (contained)
+                                return true;
+                        }
+                        return false;
+                    }
+                    return false;
+
+                }
+                return false;
+            }
+        }
+
+        private void AddTrieNode(TrieNode trieNode, string value)
+            {
+                if (string.IsNullOrEmpty(value))
+                    return;
+                trieNode.actualValue = value.ToCharArray()[0];
+                if (value == "*")
+                {
+                    return;
+                }
+                if (value.Length == 1 ) { 
+                trieNode.listTrieNodes.Add(new TrieNode
+                {
+                    actualValue = '*'
+                });
+                }
+                else if (value.Length > 1)
+                {
+                    TrieNode node = new TrieNode();
+                    AddTrieNode(node, value.Substring(1));
+                    trieNode.listTrieNodes.Add(node);
+                }
+
+            }
+    }
+}
---
+++ Trie/TrieTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrieTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TrieStructure _trieStructure = new TrieStructure();
+
+        [TestInitialize]
+        public void ResetTrie()
+        {
+            _trieStructure = new TrieStructure();
+        }
+
+        [TestMethod]
+        public void ReadWordEmpty()
+        {
+            AssertContains("");
+        }
+
+        [TestMethod]
+        public void SearchWordthatDoesNotExist()
+        {
+            AssertNotContains("casa");
+        }
+
+        [TestMethod]
+        public void InsertOneWordAndSearchIt()
+        {
+            _trieStructure.Insert("c");
+            AssertContains("c");
+        }
+
+        [TestMethod]
+        public void InsertTwoWordsAndSearchThem()
+        {
+            _trieStructure.Insert("a");
+            _trieStructure.Insert("c");
+            AssertContains("c");
+            AssertContains("a");
+        }
+
+        [TestMethod]
+        public void InsertTwoLetterWordAndSearch()
+        {
+            _trieStructure.Insert("ca");
+            AssertContains("ca");
+            AssertNotContains("cb");
+        }
+
+        private void AssertContains(string word)
+        {
+            Assert.AreEqual(_trieStructure.Contains(word), true);
+        }
+
+        private void AssertNotContains(string word)
+        {
+            Assert.AreEqual(_trieStructure.Contains(word), false);
+        }
+    }
+}
